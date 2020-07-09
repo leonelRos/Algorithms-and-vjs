f3e346705eef7cbe0a1252014ced3a390018d083
Leonel Rosales <rleonel426@gmail.com>
2020-07-08 17:19:17 -0700

    adding more challenges

--- main.js
+++ main.js
@@ -437,5 +437,5 @@ var moveZeros = function (arr_1) {
     return final_array;
     
 }
-console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]))
-console.log(moveZeros([ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ]))
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"])) //[false, 1, 1, 2, 1, 3, "a", 0, 0]
+console.log(moveZeros([ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])) //[1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
